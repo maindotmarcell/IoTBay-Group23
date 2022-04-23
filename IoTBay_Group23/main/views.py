5aee982ad67efba5b53a6ccb4708a1d1e8c87e0b
Joy Liu <13605961@student.uts.edu.au>
2022-04-23 19:29:29 +1000

    functional edit user information page

--- IoTBay_Group23/main/views.py
+++ IoTBay_Group23/main/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import redirect, render
-
+from django.contrib import messages
 # from django.http import HttpResponse
 # from .models import Customer
 # from django.contrib.auth import login, authenticate, logout
 # from django.contrib.auth.models import User
-from .forms import RegisterForm
+from .forms import RegisterForm, UpdateForm
 
 
 # Create your views here.
@@ -49,4 +49,20 @@ def logout(response):
 
 
 def edit(response):
-    return render(response, "main/edit.html", {})
+
+    if response.method == "POST":   
+        user_form = UpdateForm(response.POST, instance=response.user)
+        if user_form.is_valid():
+            user_form.save()
+            messages.success(response, f'Details Updated')
+            return redirect ("/welcome")
+
+    else:
+        user_form = UpdateForm(instance=response.user)
+
+
+    context = {
+        'user_form': user_form
+    }
+
+    return render(response, "main/edit.html", context)
