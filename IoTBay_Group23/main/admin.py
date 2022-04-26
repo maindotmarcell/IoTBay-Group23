c2b0e293883c5e4cb5df63c6a024bf648bcb992c
Marcell Munnich <marcellmunnich@gmail.com>
2022-04-26 18:19:27 +1000

    updated the models + reset and migrated database

--- IoTBay_Group23/main/admin.py
+++ IoTBay_Group23/main/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Customer,Item,Order,Payment, Invoice, Shipping
+from .models import Item,Order,Payment, Invoice, Shipping
 # Register your models here.
-admin.site.register(Customer)
 admin.site.register(Item)
 admin.site.register(Order)
 admin.site.register(Payment)
--- IoTBay_Group23/main/models.py
+++ IoTBay_Group23/main/models.py
@@ -1,12 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Customer(models.Model):
-    name = models.CharField(max_length=200)
-    password = models.CharField(max_length=40)
 
-    def __str__(self):
-        return self.name
+# Customer is a proxy class to the built in User object, this gives us the convenience of the bult in authentication functions, but gives us the flexibility to add our 
+# class Customer(models.Model):
+#     # name = models.CharField(max_length=200)
+#     # password = models.CharField(max_length=40)
+#     # email = models.EmailField(verbose_name="email", max_length=60, unique=True)
+#     # username = models.CharField(max_length=30, unique=True)
+#     # date_joined = models.DateTimeField(verbose_name="date joined", auto_now_true=True)
+#     # last_login = models.DateTimeField(verbose_name="last login", auto_now=True)
+#     # is_admin = models.BooleanField(default=False)
+#     # is_active = models.BooleanField(default=True)
+#     # is_staff = models.BooleanField(default=False)
+#     # is_superuser = models.BooleanField(default=False)
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
 class Item(models.Model):
@@ -18,8 +27,9 @@ class Item(models.Model):
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    description = models.CharField(max_length=150)
     date = models.DateTimeField()
 
     def __str__(self):
@@ -32,23 +42,21 @@ class Payment(models.Model):
 
     def __str__(self):
         return self.amount
-   
+
 
 class Invoice(models.Model):
-    UserID = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    UserID = models.ForeignKey(User, on_delete=models.CASCADE)
     PaymentID = models.ForeignKey(Payment, on_delete=models.CASCADE)
     invoice_Number = models.CharField(max_length=100)
 
     def __str__(self):
         return self.invoice_Number
 
-    
 
 class Shipping(models.Model):
-    UserID = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    UserID = models.ForeignKey(User, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    Tracking_Number = models.CharField(max_length=100)
+    Tracking_Number = models.BigIntegerField(unique=True)
 
     def __str__(self):
         return self.Tracking_Number
-
