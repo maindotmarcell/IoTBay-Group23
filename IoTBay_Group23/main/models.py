2c32f6333592650d3ae385c70ecb09c2d99f5f82
Marcell Munnich <marcellmunnich@gmail.com>
2022-05-11 13:03:25 +1000

    fixed add to cart bug

--- IoTBay_Group23/main/models.py
+++ IoTBay_Group23/main/models.py
@@ -37,6 +37,18 @@ class Order(models.Model):
 
     def __str__(self):
         return self.id
+    
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total 
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
 
 
 class Payment(models.Model):
