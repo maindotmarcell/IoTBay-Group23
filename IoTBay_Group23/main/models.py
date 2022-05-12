8eb7966fdffd6edfd941b777e8be415852e41dbe
Jared Mayger <jaredmayger@gmail.com>
2022-05-12 18:58:37 +1000

    no message

--- IoTBay_Group23/main/models.py
+++ IoTBay_Group23/main/models.py
@@ -53,11 +53,11 @@ class Order(models.Model):
 
 class Payment(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
     name_on_card = models.CharField(max_length=100)
     card_number = models.IntegerField(default=0000-0000-0000)
     expiry_date = models.DateTimeField(null=True)
     cvv = models.IntegerField(default=000)
-    amount = models.DecimalField(max_digits=7, decimal_places=3)
 
     def __str__(self):
         return self.amount
